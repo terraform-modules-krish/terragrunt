888eec6af8794a11c6cc5dbd5745bc21aa435ae3
repo-copier <info@gruntwork.io>
2023-10-24 18:57:20 -0600

    Resolve references

--- cli/hclfmt.go
+++ cli/hclfmt.go
@@ -11,9 +11,9 @@ import (
 	"github.com/hashicorp/hcl2/hclwrite"
 	"github.com/mattn/go-zglob"
 
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/options"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/util"
 )
 
 // runHCLFmt recursively looks for terragrunt.hcl files in the directory tree starting at workingDir, and formats them
