ae8ef701762191e27e3aae137bd76240b68c202f
repo-copier <info@gruntwork.io>
2023-10-24 18:57:04 -0600

    Resolve references

--- config/config_helpers_test.go
+++ config/config_helpers_test.go
@@ -7,9 +7,9 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/options"
-	"github.com/gruntwork-io/terragrunt/test/helpers"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/test/helpers"
 	"github.com/stretchr/testify/assert"
 )
 
