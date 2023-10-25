43acb783176c6b3145a9532531caa770366dd6e0
repo-copier <info@gruntwork.io>
2023-10-24 19:04:12 -0600

    Resolve references

--- cli/tfsource/types_test.go
+++ cli/tfsource/types_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/options"
 )
 
 func TestSplitSourceUrl(t *testing.T) {
