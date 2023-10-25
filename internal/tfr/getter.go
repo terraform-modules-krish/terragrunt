6b25673b5f43bc9e5f3749dbd2858ac760156a9e
repo-copier <info@gruntwork.io>
2023-10-24 19:00:39 -0600

    Resolve references

--- internal/tfr/getter.go
+++ internal/tfr/getter.go
@@ -15,8 +15,8 @@ import (
 	"github.com/hashicorp/go-getter"
 	safetemp "github.com/hashicorp/go-safetemp"
 
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/util"
 )
 
 // httpClient is the default client to be used by HttpGetters.
