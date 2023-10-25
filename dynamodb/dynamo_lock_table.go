40ae4c83723465439d2297991eff6839fea461c6
repo-copier <info@gruntwork.io>
2023-10-24 18:56:31 -0600

    Resolve references

--- dynamodb/dynamo_lock_table.go
+++ dynamodb/dynamo_lock_table.go
@@ -5,10 +5,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/gruntwork-io/terragrunt/aws_helper"
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/options"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/aws_helper"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/util"
 	"time"
 )
 
