c01dedb6b314ab827dbdc959d87bd082a8e6d6a1
repo-copier <info@gruntwork.io>
2023-10-24 18:58:29 -0600

    Resolve references

--- configstack/graph.go
+++ configstack/graph.go
@@ -1,8 +1,8 @@
 package configstack
 
 import (
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/util"
 )
 
 // Check for dependency cycles in the given list of modules and return an error if one is found
