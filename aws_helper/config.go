db689f3465ff786f494f9842dcf8cd3c4367e3bd
repo-copier <info@gruntwork.io>
2023-10-24 18:56:38 -0600

    Resolve references

--- aws_helper/config.go
+++ aws_helper/config.go
@@ -7,8 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sts"
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/options"
 	"time"
 )
 
