576f51448ce5ec42f14dd9b58ea69b291e32b50b
repo-copier <info@gruntwork.io>
2023-10-24 19:00:26 -0600

    Resolve references

--- test/integration_local_dev_test.go
+++ test/integration_local_dev_test.go
@@ -7,7 +7,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
