e794c528d27a896cfffd4dc11723d5a9959345cb
repo-copier <info@gruntwork.io>
2023-10-24 18:55:29 -0600

    Resolve references

--- cli/version_check_test.go
+++ cli/version_check_test.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	"github.com/gruntwork-io/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/errors"
 	"github.com/hashicorp/go-version"
 	"github.com/stretchr/testify/assert"
 	"testing"
