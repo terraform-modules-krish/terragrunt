f0fd40bdcc9976bf7d480f0990c93129ea3a2995
repo-copier <info@gruntwork.io>
2023-10-24 19:01:34 -0600

    Resolve references

--- cli/cli_app_test.go
+++ cli/cli_app_test.go
@@ -3,10 +3,10 @@ package cli
 import (
 	"testing"
 
-	"github.com/gruntwork-io/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/errors"
 
-	"github.com/gruntwork-io/terragrunt/config"
-	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/config"
+	"github.com/terraform-modules-krish/terragrunt/options"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
