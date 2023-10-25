e65518f3cc5eff0387a3448a1196233530f2b562
repo-copier <info@gruntwork.io>
2023-10-24 19:03:06 -0600

    Resolve references

--- cli/validate_inputs.go
+++ cli/validate_inputs.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/google/shlex"
 
-	"github.com/gruntwork-io/terragrunt/config"
-	"github.com/gruntwork-io/terragrunt/options"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/config"
+	"github.com/terraform-modules-krish/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/util"
 )
 
 const validateInputsHelp = `
