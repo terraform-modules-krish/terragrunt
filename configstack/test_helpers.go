5911e2164d81ecd007a07794ec7e1a1fc316ec2e
repo-copier <info@gruntwork.io>
2023-10-24 19:02:21 -0600

    Resolve references

--- configstack/test_helpers.go
+++ configstack/test_helpers.go
@@ -4,9 +4,9 @@ import (
 	"sort"
 	"testing"
 
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/options"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/util"
 	"github.com/hashicorp/go-multierror"
 	"github.com/stretchr/testify/assert"
 )
