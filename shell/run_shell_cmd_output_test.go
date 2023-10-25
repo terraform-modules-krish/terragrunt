8b073c531f8952b8865d0b6d1d48e77e1c4104f0
repo-copier <info@gruntwork.io>
2023-10-24 18:57:02 -0600

    Resolve references

--- shell/run_shell_cmd_output_test.go
+++ shell/run_shell_cmd_output_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/options"
 	"github.com/stretchr/testify/assert"
 )
 
