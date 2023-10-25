99583ec6fda429036cc2d001b36d2bca8b7b7a1c
repo-copier <info@gruntwork.io>
2023-10-24 18:55:34 -0600

    Resolve references

--- cli/download_source_test.go
+++ cli/download_source_test.go
@@ -2,8 +2,8 @@ package cli
 
 import (
 	"fmt"
-	"github.com/gruntwork-io/terragrunt/options"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/util"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/url"
