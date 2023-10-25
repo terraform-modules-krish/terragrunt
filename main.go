8aa24035f7097f276a79f457dc94630f41609c48
repo-copier <info@gruntwork.io>
2023-10-24 18:58:03 -0600

    Resolve references

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 
 	"github.com/fatih/color"
 
-	"github.com/gruntwork-io/terragrunt/cli"
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/shell"
-	"github.com/gruntwork-io/terragrunt/util"
+	"github.com/terraform-modules-krish/terragrunt/cli"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/shell"
+	"github.com/terraform-modules-krish/terragrunt/util"
 )
 
 // This variable is set at build time using -ldflags parameters. For more info, see:
