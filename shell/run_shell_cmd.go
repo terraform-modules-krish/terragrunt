a17a2a17cb464ec05f61de4672bd9e7651642e2f
repo-copier <info@gruntwork.io>
2023-10-24 18:56:23 -0600

    Resolve references

--- shell/run_shell_cmd.go
+++ shell/run_shell_cmd.go
@@ -10,8 +10,8 @@ import (
 	"syscall"
 
 	"bytes"
-	"github.com/gruntwork-io/terragrunt/errors"
-	"github.com/gruntwork-io/terragrunt/options"
+	"github.com/terraform-modules-krish/terragrunt/errors"
+	"github.com/terraform-modules-krish/terragrunt/options"
 )
 
 // Run the specified shell command with the specified arguments. Connect the command's stdin, stdout, and stderr to
